36ee94e98e909c49f1b1200200e16ec044f08703
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-05-21 23:22:27 +0300

    Fix warnings
    PR-URL: https://github.com/metarhia/metadomain/pull/5

--- schemas/Account.js
+++ schemas/Account.js
@@ -6,8 +6,8 @@
   active: { type: 'boolean', default: true },
   unit: { many: 'Unit' },
   roles: { many: 'Role' },
-  email: { type: 'strring', length: { min: 6, max: 255 }, unique: true },
-  phone: { type: 'strring', length: { min: 10, max: 15 }, unique: true },
+  email: { type: 'string', length: { min: 6, max: 255 }, unique: true },
+  phone: { type: 'string', length: { min: 10, max: 15 }, unique: true },
 
   fullName: {
     given: '?string',
